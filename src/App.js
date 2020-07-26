53a85ed024b3c3ab91a96f60a52cad34b45b72bb
jbuyu <jbuyu@ymail.com>
2020-07-26 18:29:49 +0300

    adds css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
