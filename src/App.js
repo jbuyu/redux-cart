2f65cd4e34b60f2676cd37875e9ac91718f80987
jbuyu <jbuyu@ymail.com>
2020-07-26 17:50:57 +0300

    removes weather app, sets up semantic ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import "./App.css";
-import { Realtime } from "./realtime.component";
-import { Hourly } from "./hourly.component";
-import { useHourly } from "./use-hourly";
-import { useRealtime } from "./use-realtime";
-import ClimacellIcon from "../icons/climacell-icon-colored.svg";
-import PinIcon from "../icons/pin.svg";
-import { addHours } from "../utilities";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Miami</h1>
+    <div className="app-root">
+      <p>miami</p>
     </div>
   );
 }
