025ff9b44ac9751314a63bd32794f03ec4843f0f
jbuyu <jbuyu@ymail.com>
2020-07-26 17:15:18 +0300

    adds hourly and realtime hooks + components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import { Realtime } from "./realtime.component";
+import { Hourly } from "./hourly.component";
+import { useHourly } from "./use-hourly";
+import { useRealtime } from "./use-realtime";
+import ClimacellIcon from "../icons/climacell-icon-colored.svg";
+import PinIcon from "../icons/pin.svg";
+import { addHours } from "../utilities";
 function App() {
   return (
     <div className="App">
