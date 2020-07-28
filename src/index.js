9dea460c460ae3fbdfae9a3ecb8bafc4116e8521
jbuyu <jbuyu@ymail.com>
2020-07-28 11:23:50 +0300

    adds browser roter, hooks up redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
+import { BrowserRouter, Switch, Route } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
