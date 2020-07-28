0d988f1b7c00c677ed670c3dcb9181c4df48288d
jbuyu <jbuyu@ymail.com>
2020-07-28 18:22:52 +0300

    misc styling on paragraphs

--- src/features/product-listing/product-list-item.js
+++ src/features/product-listing/product-list-item.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 
 export default function ProductListItem(props) {
-  return <div className="product-list-item">
-    <h5>{props.product.name}</h5>
-    <img
-      height={100}
-      src={`/products/${props.product.image}`}
-      alt="product image"
-    />
-    <div>{props.product.description}</div>
-    <div> ${props.product.price}</div>
-    <div>
-      <button>Add to Cart</button>
+  return (
+    <div className="product-list-item">
+      <h5>{props.product.name}</h5>
+      <img
+        height={100}
+        src={`/products/${props.product.image}`}
+        alt="product image"
+      />
+      <div className="description">{props.product.description}</div>
+      <div className="price"> ${props.product.price}</div>
+      <div>
+        <button>Add to Cart</button>
+      </div>
     </div>
-  </div>
-
+  );
 }
