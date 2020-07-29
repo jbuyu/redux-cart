18af8d9b9c63ac77438a05af71d5064113f8c39b
jbuyu <jbuyu@ymail.com>
2020-07-29 22:19:06 +0300

    ads cart page

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-
+import Cart from "../features/cart";
 export default function CartPage() {
-  return <div>Cart Page</div>;
+  return (
+    <div className="cart">
+      <Cart />
+    </div>
+  );
 }
